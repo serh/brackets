976022167c2496199978755ddee009d855952b3f
serh <serh169@gmail.com>
2019-10-03 00:06:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+
+    let stack = [];
+    let splittedArray = str.split("");
+
+    if (splittedArray.length % 2 !== 0) {
+        return false;
+    }
+
+    splittedArray.forEach((current) => {
+        let lastAdded = stack[stack.length-1];
+        bracketsConfig.forEach(element => {
+            if(lastAdded === element[0] && current === element[1]) {
+                stack.pop();
+            } else if (current === element[0]) {
+                stack.push(current);
+            }
+        });
+    });
+
+    return !stack.length;
+
+};
